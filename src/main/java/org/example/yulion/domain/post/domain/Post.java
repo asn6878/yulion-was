a112d8a93fc59133615613a84d509998ee44dd3c
asn6878 <asn6878@gmail.com>
2024-01-29 02:27:34 +0900

    feat[#3]: Entity 작성

---
+++ src/main/java/org/example/yulion/domain/post/domain/Post.java
@@ -0,0 +1,47 @@
+package org.example.yulion.domain.post.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.yulion.domain.category.domain.Category;
+import org.example.yulion.domain.common.BaseTimeEntity;
+import org.example.yulion.domain.part.domain.Part;
+import org.example.yulion.domain.user.domain.User;
+
+@Entity
+@Table
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User writer;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Category category;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Part part;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String content;
+    private Long viewCnt;
+
+    @Column(nullable = true)
+    private String members;
+
+    @Builder
+    protected Post (String title, String content, Long viewCnt, String members){
+        this.title = title;
+        this.content = content;
+        this.viewCnt = viewCnt;
+        this.members = members;
+    }
+
+}
