50faeb8032102b97a676e65c968e586f074def55
asn6878 <asn6878@gmail.com>
2024-02-26 22:26:40 +0900

    fix[#3]: 게시물 카테고리, 파트 열거형으로 변경

--- src/main/java/org/example/yulion/domain/post/dto/response/PostEducationSummaryResponse.java
+++ src/main/java/org/example/yulion/domain/post/dto/response/PostEducationSummaryResponse.java
@@ -1,12 +1,13 @@
 package org.example.yulion.domain.post.dto.response;
 
+import org.example.yulion.domain.post.domain.Part;
 import org.example.yulion.domain.post.domain.Post;
 
 import java.time.LocalDateTime;
 
 public record PostEducationSummaryResponse(
         Long id,
-        String part,
+        Part part,
         String title,
         String members,
         String mentor) {
@@ -14,7 +15,7 @@ public record PostEducationSummaryResponse(
     public static PostEducationSummaryResponse from(Post post) {
         return new PostEducationSummaryResponse(
                 post.getId(),
-                post.getPart().getName(),
+                post.getPart(),
                 post.getTitle(),
                 post.getMembers(),
                 post.getMentor()
