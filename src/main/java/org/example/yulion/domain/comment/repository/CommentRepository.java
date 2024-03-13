43b3f3433182b52414f2c4c318fb538379989ba2
asn6878 <asn6878@gmail.com>
2024-03-13 15:33:17 +0900

    feat/#14/comment : 댓글 조회시 대댓글 비반환 처리

--- src/main/java/org/example/yulion/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/yulion/domain/comment/repository/CommentRepository.java
@@ -5,9 +5,11 @@ import org.example.yulion.domain.user.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+        @Query("SELECT c FROM Comment c WHERE c.parentComment IS NULL ORDER BY c.createAt DESC")
         Page<Comment> findAllByWriterOrderByCreatedAt(User user, Pageable pageable);
 }
