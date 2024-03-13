8f2f6922408770784adfdbeab183a32bb93f9e45
asn6878 <asn6878@gmail.com>
2024-03-13 16:01:27 +0900

    feat/#14/dto : 댓글 응답 DTO 수정 (대댓글 반영)

--- src/main/java/org/example/yulion/domain/comment/dto/response/CommentResponse.java
+++ src/main/java/org/example/yulion/domain/comment/dto/response/CommentResponse.java
@@ -1,19 +1,41 @@
 package org.example.yulion.domain.comment.dto.response;
 
+import lombok.Builder;
+import lombok.Setter;
 import org.example.yulion.domain.comment.domain.Comment;
+import org.example.yulion.domain.comment.domain.CommentStatus;
 
-public record CommentResponse (
-        Long id,
-        Long userId,
-        String content,
-        Long parentCommentId
-){
-    public static CommentResponse from(Comment comment){
-        return new CommentResponse(
-                comment.getId(),
-                comment.getWriter().getId(),
-                comment.getContent(),
-                comment.getParentComment().getId()
-        );
+import java.util.List;
+
+@Setter
+public class CommentResponse {
+    private final Long id;
+    private final Long userId;
+    private final String content;
+    private final Long parentCommentId;
+    private final List<Comment> childComments;
+
+    @Builder
+    public CommentResponse(Long id, Long userId, String content, Long parentCommentId, List<Comment> childComments) {
+        this.id = id;
+        this.userId = userId;
+        this.content = content;
+        this.parentCommentId = parentCommentId;
+        this.childComments = childComments;
+    }
+
+    public static CommentResponse from(Comment comment) {
+        CommentResponseBuilder builder = CommentResponse.builder()
+                .id(comment.getId())
+                .userId(comment.getWriter().getId())
+                .content(comment.getContent())
+                .parentCommentId(comment.getParentComment().getId())
+                .childComments(comment.getChildComments());
+
+        if (comment.getStatus() == CommentStatus.DELETED) {
+            builder.content("삭제된 댓글입니다.");
+        }
+
+        return builder.build();
     }
 }
